14404eeed2173c7285f6f6b87048a15afc203d70
yashu <sudhir.jigyasu93@gmail.com>
2022-07-07 15:33:11 +0530

    deleted class fle

--- src/main/java/guru/springframework/SpringBootMySqlApplication.java
+++ src/main/java/guru/springframework/SpringBootMySqlApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootMySqlApplication {
 
 	public static void main(String[] args) {
+		System.out.print("inside main class");	
 		SpringApplication.run(SpringBootMySqlApplication.class, args);
 	}
 }
