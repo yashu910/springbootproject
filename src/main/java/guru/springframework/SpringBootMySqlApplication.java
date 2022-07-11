e94fca7c2f87fbf24bab9ad24935aafc87e22fa8
yashu <sudhir.jigyasu93@gmail.com>
2022-07-11 12:21:52 +0530

    build fail forcefully

--- src/main/java/guru/springframework/SpringBootMySqlApplication.java
+++ src/main/java/guru/springframework/SpringBootMySqlApplication.java
@@ -3,8 +3,8 @@ package guru.springframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
-public class SpringBootMySqlApplication {
+@SpringBootApplication
+public  SpringBootMySqlApplication {
 
 	public static void main(String[] args) {
 		System.out.print("inside main class");	
