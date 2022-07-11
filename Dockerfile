1021a1b859dc83c7f893e9b2308cabdcb7de8c2c
yashu <sudhir.jigyasu93@gmail.com>
2022-07-11 12:19:06 +0530

    build fail forcefully

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 From openjdk:8
 copy ./target/app.jar app.jar
 CMD ["java","-jar","app.jar"]
+
+
--- src/main/java/guru/springframework/SpringBootMySqlApplication.java
+++ src/main/java/guru/springframework/SpringBootMySqlApplication.java
@@ -3,7 +3,7 @@ package guru.springframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringBootMySqlApplication {
 
 	public static void main(String[] args) {
