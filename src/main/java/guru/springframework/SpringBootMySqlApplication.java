6e79f7607c949188a978ed77abb2b3cb558f0876
yashu <sudhir.jigyasu93@gmail.com>
2022-07-11 12:26:34 +0530

    resolved failre build

--- src/main/java/guru/springframework/SpringBootMySqlApplication.java
+++ src/main/java/guru/springframework/SpringBootMySqlApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public  SpringBootMySqlApplication {
+public class SpringBootMySqlApplication {
 
 	public static void main(String[] args) {
 		System.out.print("inside main class");	
